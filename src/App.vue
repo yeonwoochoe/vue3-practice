364221e4bc3da49e4ec01f9ded72224fff1d97e4
yeonwoo <chiyy6162@gmail.com>
2022-04-20 22:11:35 +0900

    vue3-practice

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="one($event), two($event)">Submit</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    one(event) {
+      console.log(event);
+    },
+    two(event) {
+      console.log(event);
+    },
+  },
+};
+</script>
