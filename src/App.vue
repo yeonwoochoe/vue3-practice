8726d5cf41674f74f6a42ba4151d9580ce0d88e2
yeonwoo <chiyy6162@gmail.com>
2022-04-21 18:38:28 +0900

    컴포넌트 기초와 props 전달하기 및 slot  개념

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <input type="text" @keydown.ctrl.shift.a="handler" />
+  <MyBtn text="포도" style="border-radius: 30px" />
+  <MyBtn color="red">사과</MyBtn>
+  <MyBtn color="#404040">딸기</MyBtn>
+  <MyBtn :color="color">파인애플</MyBtn>
+  <MyBtn large color="royalblue">블루베리</MyBtn>
+  <MyBtn>Apple</MyBtn>
+  <MyBtn :color="color"><span style="color: red">lemon</span></MyBtn>
+
+  <button>Banana</button>
 </template>
 
 <script>
+//부모자식간의 데이터 통신
+import MyBtn from "~/components/MyBtn.vue";
+
 export default {
-  methods: {
-    handler() {
-      console.log("Enter!");
-    },
+  components: {
+    MyBtn,
+  },
+  data() {
+    return {
+      color: "#000",
+    };
   },
 };
 </script>
---
+++ src/components/MyBtn.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="{ large }" :style="{ backgroundColor: color }" class="btn">
+    <!-- {{ text }} -->
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "gray",
+    },
+    large: {
+      type: Boolean,
+      default: false,
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.btn {
+  display: inline-block;
+  margin: 4px;
+  padding: 6px 12px;
+  border-radius: 4px;
+  background-color: gray;
+  color: white;
+  cursor: pointer;
+  &.large {
+    font-size: 15px;
+    padding: 15px;
+    font-weight: 800;
+  }
+}
+</style>
